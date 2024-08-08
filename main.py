55f4dcbfca37bae0d7bb200b191507806e0b3049
maliha shahi <maliha.shahi.03@gmail.com>
2024-08-08 23:23:28 +0430

    more option of tkineter

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from tkinter import  *
 
 
 window = Tk()
-window.title("instagram profile downloader")
+window.geometry("600x600")
+window.maxsize(800,800)
+window.minsize(300,300)
+window.title("Profile downloader")
+#label
+label = Label(window, text ="Hello And Welcome" ,fg="blue",bg="black")
+label.pack()
+#button
+def hello():
+    print(input.get())
+    button.config(text="hello python GUI")
+button =Button(window,text="Click Here",fg="red" ,bg="yellow",command=hello)
+button.place(x=30,y=70)
+#input
+input =Entry(window)
+input.pack()
 
+window.mainloop()
 window.mainloop()
