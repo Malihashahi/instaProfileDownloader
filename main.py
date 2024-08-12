c80af2922771ae6baebebbaa6fff47cfa779ae54
maliha shahi <maliha.shahi.03@gmail.com>
2024-08-12 23:32:56 +0430

    use urllib ,pil , io with instaloader to complete project

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-import instaloader
+from  tkinter import *
+import  instaloader
+import  urllib
+from  urllib.request import urlopen
+from  PIL import ImageTk
+import  io
+def get_image():
+    L = instaloader.Instaloader()
+    profile =instaloader.profile.from_username(L.context,f"{username.get()}")
+    a= urlopen(profile.get_profile_pic_url()
+    data= a.read()
+    a.close()
+    image= Image.open(io.BytesIO(data))
+    pic = ImageTk.PhotoImage(image)
+    label.config(image=pic)
+    label.image= pic
+    label.pack()
 
-# Get instance
-loader = instaloader.Instaloader()
 
-# Login using the credentials
-loader.login(USER, PASSWORD)
 
-# Use Profile class to access metadata of account
-profile = instaloader.Profile.from_username(loader.context,'maliha_baharS')
+window =Tk()
+window.title("instagram profile downloader")
+window.geometry("600x600")
+Label(window,text="Enter your instagram username").pack()
+username =Entry(window,width=50)
+username.pack()
+button= Button(window,text="start download")
+button.pack()
+button.config(command=get_image)
+label= Label(window)
+
+
+
+window.mainloop()
